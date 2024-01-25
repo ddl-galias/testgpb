243153bdb24f71766ba06c1873e37810c08bd71f
ddl-galias <142441807+ddl-galias@users.noreply.github.com>
2024-01-25 10:37:39 -0300

    ssss

---
+++ test-with-output.sh
@@ -0,0 +1,105 @@
+FILE_CONTENT="
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at efficitur arcu, vitae pellentesque libero. Nulla consequat elit sed diam tincidunt, semper convallis mi consectetur. Aenean facilisis sodales ex nec bibendum. Vestibulum condimentum, dui sit amet maximus feugiat, justo tortor luctus erat, quis laoreet tortor nunc sit amet erat. Nullam diam erat, sollicitudin ornare molestie sit amet, iaculis nec ligula. Morbi at accumsan nibh. In ornare porta metus. Nulla venenatis eros et massa feugiat fringilla.
+
+Sed vitae efficitur ligula, in iaculis lorem. Sed sit amet nunc sit amet mauris pulvinar facilisis id ut augue. Donec dapibus dolor et lectus ultrices consequat. Vivamus in bibendum mauris. Curabitur pulvinar purus nulla, vitae tristique nunc maximus eget. Nam quis nisl quis dolor faucibus congue in sodales odio. Cras eu justo scelerisque, ultricies massa vel, egestas ante. Nunc rutrum est erat, a placerat dui rutrum sit amet. Praesent quis pretium risus. Curabitur ultricies enim enim, sed fermentum elit finibus vel. Donec quis tortor laoreet, mollis purus in, tempus mauris. Praesent condimentum nulla augue, at tincidunt lectus mollis ut. Sed vel arcu a velit lacinia rhoncus. Fusce ullamcorper iaculis tincidunt. Ut ultrices hendrerit ligula ut accumsan. Aliquam non justo nec libero consequat ornare at et leo.
+
+Nam tempus nulla tristique risus posuere semper. Ut sed aliquet justo. Aenean accumsan blandit est at aliquam. Morbi sed gravida dui, id volutpat ex. Ut viverra mi id pellentesque lacinia. Cras sodales, libero at ultricies aliquet, odio magna porttitor tellus, vel rutrum sapien ligula vel magna. Etiam ultrices tempus suscipit. Nunc lorem velit, vestibulum vitae lacus at, mollis malesuada lectus. Curabitur quis dictum leo, ut fringilla elit. Nulla faucibus fringilla mollis. Praesent pretium tincidunt dignissim. Nam eget lacus ut neque volutpat varius tristique a ex. Mauris in dignissim lacus, a sagittis est. Nullam consectetur tincidunt libero sit amet convallis.
+
+Vestibulum tempor mi diam, vel tincidunt nisi blandit eget. Nullam lacinia pretium diam sit amet placerat. Donec et luctus mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed vel est vel orci faucibus blandit. Nullam in nisi lectus. Maecenas quis iaculis lacus. Sed ornare, turpis sit amet facilisis malesuada, nisl purus pretium urna, id aliquam augue ante quis urna. Duis quis rhoncus neque. Aliquam auctor gravida luctus.
+
+Curabitur blandit ex augue, vitae suscipit orci dictum vel. Maecenas suscipit maximus vestibulum. Sed suscipit auctor mi, id tempor ligula semper at. Nam malesuada, velit at vestibulum vehicula, eros enim varius augue, ac rhoncus augue est et neque. Quisque euismod dolor semper blandit lobortis. Maecenas vitae pharetra eros. Sed efficitur hendrerit est sed pellentesque. Sed tristique pharetra tellus, ac consectetur massa tincidunt et. Integer efficitur ante scelerisque dolor eleifend, id bibendum dui rutrum. Fusce ornare massa non felis malesuada, vel dapibus libero iaculis. Nam arcu mauris, venenatis sagittis sem in, porttitor condimentum ante.
+
+Ut eu rutrum odio. Suspendisse bibendum imperdiet dolor, ac maximus eros condimentum id. Duis ut neque convallis, tristique urna ut, molestie turpis. Quisque efficitur lectus non odio luctus aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet eros sit amet mauris sollicitudin gravida. Aenean sit amet neque velit. Aenean nec nisi tincidunt, eleifend leo eget, mattis orci. Donec vulputate mi sit amet luctus lacinia. Vestibulum vitae semper erat, nec commodo erat.
+
+Suspendisse quis justo quis purus vulputate lobortis. Cras finibus suscipit sapien, quis ullamcorper massa luctus eu. Nam pellentesque faucibus augue, ac convallis diam. Ut quis augue mauris. Nullam lobortis tellus quis lorem sollicitudin lobortis. Etiam ac pretium ipsum. Phasellus vel pellentesque mauris, nec bibendum massa. Quisque lacinia efficitur sapien. Sed euismod nibh a sollicitudin bibendum. Pellentesque semper libero risus, quis rhoncus ex auctor a. Donec nunc erat, ultrices eu gravida vitae, sollicitudin nec ex. Vivamus ante felis, malesuada porttitor elit a, tincidunt egestas eros. Vivamus sed quam tincidunt, facilisis velit at, ornare velit. Aenean fermentum pellentesque velit, sit amet sollicitudin nulla convallis sed.
+"
+
+echo $FILE_CONTENT > ./output-file-1.txt
+echo generated file 1
+
+echo $FILE_CONTENT > ./output-file-2.txt
+echo generated file 2
+
+echo $FILE_CONTENT > ./output-file-3.txt
+echo generated file 3
+
+echo $FILE_CONTENT > ./output-file-4.txt
+echo generated file 4
+
+echo $FILE_CONTENT > ./output-file-5.txt
+echo generated file 5
+
+echo $FILE_CONTENT > ./output-file-6.txt
+echo generated file 6
+
+echo $FILE_CONTENT > ./output-file-7.txt
+echo generated file 7
+
+echo $FILE_CONTENT > ./output-file-8.txt
+echo generated file 8
+
+echo $FILE_CONTENT > ./output-file-9.txt
+echo generated file 9
+
+echo $FILE_CONTENT > ./output-file-10.txt
+echo generated file 10
+
+echo $FILE_CONTENT > ./output-file-11.txt
+echo generated file 11
+
+echo $FILE_CONTENT > ./output-file-12.txt
+echo generated file 12
+
+echo $FILE_CONTENT > ./output-file-13.txt
+echo generated file 13
+
+echo $FILE_CONTENT > ./output-file-14.txt
+echo generated file 14
+
+echo $FILE_CONTENT > ./output-file-15.txt
+echo generated file 15
+
+echo $FILE_CONTENT > ./output-file-16.txt
+echo generated file 16
+
+echo $FILE_CONTENT > ./output-file-17.txt
+echo generated file 17
+
+echo $FILE_CONTENT > ./output-file-18.txt
+echo generated file 18
+
+echo $FILE_CONTENT > ./output-file-19.txt
+echo generated file 19
+
+echo $FILE_CONTENT > ./output-file-20.txt
+echo generated file 20
+
+echo $FILE_CONTENT > ./output-file-21.txt
+echo generated file 21
+
+echo $FILE_CONTENT > ./output-file-22.txt
+echo generated file 22
+
+echo $FILE_CONTENT > ./output-file-23.txt
+echo generated file 23
+
+echo $FILE_CONTENT > ./output-file-24.txt
+echo generated file 24
+
+echo $FILE_CONTENT > ./output-file-25.txt
+echo generated file 25
+
+echo $FILE_CONTENT > ./output-file-26.txt
+echo generated file 26
+
+echo $FILE_CONTENT > ./output-file-27.txt
+echo generated file 27
+
+echo $FILE_CONTENT > ./output-file-28.txt
+echo generated file 28
+
+echo $FILE_CONTENT > ./output-file-29.txt
+echo generated file 29
+
+echo $FILE_CONTENT > ./output-file-30.txt
+echo generated file 30
