b8369a1f1de1cfe2fa5bd0cca3fbbdce01f7cabb
ddl-galias <142441807+ddl-galias@users.noreply.github.com>
2024-06-13 14:18:40 -0300

    Add files via upload

---
+++ structured_dataset.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from flytekit import kwtypes
+
+from flytekit.types.structured.structured_dataset import (
+    PARQUET,
+    StructuredDataset,
+    StructuredDatasetDecoder,
+    StructuredDatasetEncoder,
+    StructuredDatasetTransformerEngine,
+)
+
+all_cols = kwtypes(Name=str, Age=int, Height=int)
+df = pd.DataFrame({"Name": ["Tom", "Joseph"], "Age": [36, 22], "Height": [160, 178]})
+
+sd = StructuredDataset(dataframe=df)
+
+def get_sds():
+    return sd
